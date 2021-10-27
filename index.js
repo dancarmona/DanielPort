0887d3dea7b5f251a64a3e1662ee32870bcef0cb
Daniel Carmona <dcaracos@gmail.com>
2021-10-27 14:48:18 +1100

    added without loop word changing to brand

---
+++ index.js
@@ -0,0 +1,35 @@
+
+
+// CHANGE WORLD MANY MANYTIMES HOWEVER WOULD BE BETTER FIND A LOOP TO DO THIS 
+
+$('.spanx').fadeOut(5000, function () {
+     $(this).text("Engineers").fadeIn();
+     
+   });
+
+$('.spanx').fadeIn(5000, function () {
+    $(this).text("Engineers")
+    
+  });
+  
+
+  $('.spanx').fadeOut(5000, function () {
+    $(this).text("Coders");
+    
+  });
+
+$('.spanx').fadeIn(5000, function () {
+   $(this).text("Coders")
+   
+ });
+
+
+ $('.spanx').fadeOut(5000, function () {
+    $(this).text("Architects").fadeIn();
+    
+  });
+
+$('.spanx').fadeIn(5000, function () {
+   $(this).text("Architects")
+   
+ });
